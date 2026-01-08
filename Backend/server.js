a343c4c5c583749be6d93258c12ca2310c2168b4
Aarthi Honguthi <aarthihonguthi025@gmail.com>
2026-01-08 11:32:39 +0530

    urls backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use('/api/event-types', require('./routes/eventRoutes'));
 app.use('/api/availability', require('./routes/availabilityRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
